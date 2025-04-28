242c027387e4e287b1d58dc0de6aedeec4733bb5
SmritiPiplani <smritipiplani16@gmail.com>
2025-04-28 10:40:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,21 +10,6 @@ const app = express();
 app.use(express.json());
 
 
-app.get('/test-email', async (req, res) => {
-    try {
-      await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: 'smritipiplani@gmail.com', // 💡 replace with your actual email
-        subject: 'Test Email from Nodemailer',
-        text: 'If you received this, the email config is working! 🎉',
-      });
-  
-      res.status(200).json({ success: true, message: 'Test email sent!' });
-    } catch (error) {
-      console.error('❌ Email send error:', error);
-      res.status(500).json({ success: false, message: 'Failed to send email' });
-    }
-  });
 app.use('/auth', Auth);
 
 const PORT = process.env.PORT || 5000;
